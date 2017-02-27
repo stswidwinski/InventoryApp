da220d4064fe6e477427c79acc30ebc2d555347b
cr554 <cody.roy@yale.edu>
2017-02-27 10:12:27 -0500

    Database is up and working :^)

--- app/src/main/java/com/example/cr554/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/cr554/inventoryapp/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.cr554.inventoryapp;
 
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.cr554.inventoryapp.adapter.InventoryCursorAdapter;
 import com.example.cr554.inventoryapp.database.InventoryDBHelper;
 import com.example.cr554.inventoryapp.database.InventoryContract.InventoryEntry;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor>{
     private InventoryDBHelper mDbHelper;
+    private static final int INVENTORY_LOADER =0;
+    InventoryCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +39,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //find list view
+        ListView itemListView = (ListView) findViewById(R.id.inventory_list);
+
+        //set up adatper to create list item for each row of pet data in the cursor
+        mCursorAdapter = new InventoryCursorAdapter(this, null);
+        itemListView.setAdapter(mCursorAdapter);
+
+        //set up the item click listener
+        itemListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id){
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                Uri currentItemUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI,id);
+                intent.setData(currentItemUri);
+                startActivity(intent);
+            }
+        });
+
+        //launch loader
+        getLoaderManager().initLoader(INVENTORY_LOADER,null,this);
+
         //access the database by instantiating the InventoryDBHelper subclass of SQLiteOpenHelper
         mDbHelper = new InventoryDBHelper(this);
     }
@@ -56,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
                 null,                       //Where clause column
                 null,                       //Where clause value
                 null,                       //group rows
-                null,                       //rowgroup filter
+                null,                       //row group filter
                 null);                      //sort order
         TextView dbDisplay = (TextView) findViewById(R.id.db_info);
         try{
@@ -71,5 +104,33 @@ public class MainActivity extends AppCompatActivity {
         }finally{
             cursor.close();
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {InventoryEntry._ID,
+                InventoryEntry.COLUMN_NAME,
+                InventoryEntry.COLUMN_SUPPLIER,
+                InventoryEntry.COLUMN_PRICE,
+                InventoryEntry.COLUMN_QUANTITY};
+
+        return new CursorLoader(this,               //parent activity context
+                InventoryEntry.CONTENT_URI,         //uri of current item
+                projection,                         //columns to include in the cursor
+                null,                               //null selection clause
+                null,                               //null selection arguments
+                null);                              //default sort
+    }
+
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
+
     }
 }
--- app/src/main/java/com/example/cr554/inventoryapp/database/InventoryContract.java
+++ app/src/main/java/com/example/cr554/inventoryapp/database/InventoryContract.java
@@ -1,5 +1,6 @@
 package com.example.cr554.inventoryapp.database;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -10,8 +11,12 @@ import android.provider.BaseColumns;
 public final class InventoryContract {
 
     private InventoryContract(){}
+    public static final String CONTENT_AUTHORITY = "com.example.cr554.inventoryapp";
+    public static final String PATH_INVENTORY = "inventory";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+ CONTENT_AUTHORITY);
 
     public static abstract class InventoryEntry implements BaseColumns{
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
         public static final String TABLE_NAME = "inventory";        //TEXT
         public static final String COLUMN_NAME = "name";            //TEXT
         public static final String COLUMN_PRICE = "price";          //REAL
